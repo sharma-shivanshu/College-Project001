a77ed5f9b732e3d83e5496c4dd74c3079fde3c26
uhsnavihs23 <uhsnavihs23@gmail.com>
2026-09-21 22:54:02 +0530

    fix: remove dotenv from database.py to fix github actions module error

--- src/database.py
+++ src/database.py
@@ -1,9 +1,5 @@
 import os
 from supabase import create_client
-from dotenv import load_dotenv
-load_dotenv('/Users/shivanshusharma/Documents/AGY_Projects/College-Project001/.env')
-
-
 def get_supabase_client():
     url = os.environ.get("SUPABASE_URL")
     key = os.environ.get("SUPABASE_KEY")
